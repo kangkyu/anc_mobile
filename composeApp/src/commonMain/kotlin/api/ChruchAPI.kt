1dc0d07038519ac33a94e120763dff76f6f68fa2
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-01-04 20:40:04 -0800

    Show url from response on the screen

---
+++ composeApp/src/commonMain/kotlin/api/ChruchAPI.kt
@@ -0,0 +1,28 @@
+package api
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import model.ExternalURL
+
+private const val baseUrl = "https://anc-backend-7502ef948715.herokuapp.com"
+
+class ChruchAPI {
+
+    companion object {
+        val shared = ChruchAPI()
+    }
+
+    val client = HttpClient(CIO)
+
+    suspend fun getJuboExternalURL(): Result<String> {
+        return runCatching {
+            val response: HttpResponse = client.get("${baseUrl}/jubo.json")
+            client.close()
+
+            response.body()
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/ui/JuboView.kt
+++ composeApp/src/commonMain/kotlin/ui/JuboView.kt
@@ -5,17 +5,50 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import api.ChruchAPI
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.decodeFromString
+import model.ExternalURL
 
 @Composable
 fun JuboView() {
+    var viewLink by remember { mutableStateOf(false) }
+    val externalLink = remember { mutableStateOf(ExternalURL("")) }
+
+    LaunchedEffect(Unit) {
+        try {
+            ChruchAPI.shared.getJuboExternalURL().onSuccess {
+                println(it)
+                try {
+                    externalLink.value = Json.decodeFromString<ExternalURL>(it)
+                } catch (e: Exception) {
+                    println(e)
+                }
+
+            }.onFailure {
+                println(it)
+            }
+        } catch (e: Exception) {
+            println(e)
+        }
+    }
+
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = {}) {
-            Text("Button")
+        Button(onClick = { viewLink = !viewLink }) {
+            if (viewLink) Text("Hide") else Text("Show")
+        }
+        if (viewLink) {
+            Text(externalLink.value.url)
         }
     }
 }
