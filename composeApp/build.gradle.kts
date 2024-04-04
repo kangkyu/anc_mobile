2d207af4c7f0d67827658dd65b168e84e2c27c78
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-04-04 16:32:20 -0700

    Version Code 2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -78,7 +78,7 @@ android {
         applicationId = "org.example.project"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
     }
     packaging {
